cf17e87b49c685e789e0e0ae59fd07a0be07ed74
yanglr <yangle20081982@163.com>
2024-01-28 11:58:08 +0800

    Add Autofac's test console project using keyed.

---
+++ Autofac-console-keyed-Module/Program.cs
@@ -0,0 +1,107 @@
+﻿using Autofac;
+using Autofac.Features.Indexed;
+
+namespace Autofac_console_keyed_Module
+{
+    public interface IService
+    {
+        void DoSomething();
+    }
+
+    public class ServiceA : IService
+    {
+        private readonly string _type;
+
+        public ServiceA(string type)
+        {
+            _type = type;
+            Console.WriteLine("ServiceA constructed with type: " + type);
+        }
+
+        public void DoSomething()
+        {
+            Console.WriteLine("ServiceA is doing something.");
+        }
+    }
+
+    public class ServiceB : IService
+    {
+        private readonly string _type;
+
+        public ServiceB(string type)
+        {
+            _type = type;
+            Console.WriteLine("ServiceB constructed with type: " + type);
+        }
+
+        public void DoSomething()
+        {
+            Console.WriteLine("ServiceB is doing something.");
+        }
+    }
+
+    public class ServicesModule : Autofac.Module
+    {
+        private readonly Dictionary<string, string> _typeDictionary;
+
+        public ServicesModule(Dictionary<string, string> typeDictionary)
+        {
+            _typeDictionary = typeDictionary;
+        }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<ServiceA>().Keyed<IService>("ServiceA").WithParameter((p, c) => p.Name == "type", (p, c) => _typeDictionary["ServiceA"]);
+            builder.RegisterType<ServiceB>().Keyed<IService>("ServiceB").WithParameter((p, c) => p.Name == "type", (p, c) => _typeDictionary["ServiceB"]);
+        }
+    }
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var builder = new ContainerBuilder();
+
+            var typeDictionary = new Dictionary<string, string>
+            {
+                { "ServiceA", "TypeA" },
+                { "ServiceB", "TypeB" }
+            };
+
+            builder.RegisterModule(new ServicesModule(typeDictionary));
+            builder.RegisterType<ServiceConsumer>();
+
+            var container = builder.Build();
+
+            using (var scope = container.BeginLifetimeScope())
+            {
+                var consumer = scope.Resolve<ServiceConsumer>();
+
+                string serviceKey = "ServiceA";
+                consumer.UseService(serviceKey);
+            }
+        }
+    }
+
+    public class ServiceConsumer
+    {
+        private readonly IIndex<string, IService> _services;
+
+        public ServiceConsumer(IIndex<string, IService> services)
+        {
+            _services = services;
+        }
+
+        public void UseService(string key)
+        {
+            if (_services.TryGetValue(key, out var service))
+            {
+                service.DoSomething();
+            }
+            else
+            {
+                Console.WriteLine("Unknown service key: " + key);
+            }
+        }
+    }
+}
---
+++ Autofac-console/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using Autofac;
+using Autofac.Features.Indexed;
+
+namespace Autofac_console_keyed
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var builder = new ContainerBuilder();
+
+            // 注册键控索引类型的服务
+            builder.RegisterType<ServiceA>().Keyed<IService>("ServiceA").WithParameter("type", "TypeA");      // ToDo: IService need rename
+            builder.RegisterType<ServiceB>().Keyed<IService>("ServiceB").WithParameter("type", "TypeB");
+            builder.RegisterType<ServiceConsumer>();
+
+            var container = builder.Build();
+
+            // 解析 ServiceConsumer，并注入 IIndex<string, IService>
+            using (var scope = container.BeginLifetimeScope())
+            {
+                var consumer = scope.Resolve<ServiceConsumer>(new TypedParameter(typeof(Dictionary<string, string>), GetDictionary()));
+
+                // 从其他地方获取 Service key
+                string serviceKey = "ServiceA"; // ToDo: need replace
+
+                // 使用变量作为参数调用 UseService 方法
+                consumer.UseService(serviceKey);
+                Console.ReadKey();
+            }
+        }
+
+        private static Dictionary<string, string> GetDictionary()
+        {
+            // 创建类型字典并添加对应的类型字符串
+            var typeDictionary = new Dictionary<string, string>
+            {
+                { "ServiceA", "TypeA" },
+                { "ServiceB", "TypeB" }
+            };
+
+            return typeDictionary;
+        }
+    }
+}
+
+public interface IService
+{
+    void DoSomething();
+}
+
+public class ServiceA : IService
+{
+    public ServiceA(string type)
+    {
+        Console.WriteLine("ServiceA constructed with type: " + type);
+    }
+
+    public void DoSomething()
+    {
+        Console.WriteLine("ServiceA is doing something.");
+    }
+}
+
+public class ServiceB : IService
+{
+    public ServiceB(string type)
+    {
+        Console.WriteLine("ServiceB constructed with type: " + type);
+    }
+
+    public void DoSomething()
+    {
+        Console.WriteLine("ServiceB is doing something.");
+    }
+}
+
+public class ServiceConsumer
+{
+    private readonly IIndex<string, IService> _services;
+
+    public ServiceConsumer(IIndex<string, IService> services)
+    {
+        _services = services;
+    }
+
+    public void UseService(string key)
+    {
+        if (_services.TryGetValue(key, out var service))
+        {
+            service.DoSomething();
+        }
+        else
+        {
+            Console.WriteLine("Unknown service key: " + key);
+        }
+    }
+}
